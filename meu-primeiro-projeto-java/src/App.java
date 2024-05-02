729c7e093e62b07c5f5f69dff2e6fc04f0fd7f8b
eliseu <eliseu74123@gmail.com>
2024-05-02 20:24:03 -0300

    Testando novamente o compatilhamento do codigo

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Meu primeiro hello word em java");
+        System.out.println("segundo teste");
     }
 }
